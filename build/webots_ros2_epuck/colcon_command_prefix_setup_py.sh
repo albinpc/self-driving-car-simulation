8fc311bbf498a954b36378146161c53642ddf4e5
albinpc <albin.p.cse@gmail.com>
2025-08-28 16:39:03 +0200

    Delete build directory

--- build/webots_ros2_epuck/colcon_command_prefix_setup_py.sh
+++
@@ -1,5 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/albin/webots_ws/install/webots_ros2_importer/share/webots_ros2_importer/package.sh"
-. "/home/albin/webots_ws/install/webots_ros2_msgs/share/webots_ros2_msgs/package.sh"
-. "/home/albin/webots_ws/install/webots_ros2_driver/share/webots_ros2_driver/package.sh"
-. "/home/albin/webots_ws/install/webots_ros2_control/share/webots_ros2_control/package.sh"
